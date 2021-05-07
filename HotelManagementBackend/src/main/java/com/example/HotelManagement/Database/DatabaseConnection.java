f97266295d55608c727c7548b48f056221a76da9
Bora Çün <cun2424@gmail.com>
2021-05-08 01:58:58 +0300

    User table definitions added

--- HotelManagementBackend/src/main/java/com/example/HotelManagement/Database/DatabaseConnection.java
+++ HotelManagementBackend/src/main/java/com/example/HotelManagement/Database/DatabaseConnection.java
@@ -13,7 +13,13 @@ public class DatabaseConnection {
 
     public DatabaseConnection() {}
 
-    public ResultSet execute(String query , int type ) {
+    /**
+     * This method creates a connection, and executes a query or update.
+     * @param query The desired SQL update or query as string
+     * @param type determines the type of the execution: FETCH for query, UPDATE for update
+     * @return The result of the query as a ResultSet. null if type is UPDATE.
+     */
+    public ResultSet execute(String query, int type ) {
         Connection con = null;
         Statement stmt = null;
         try {
@@ -50,6 +56,129 @@ public class DatabaseConnection {
         return null;
     }
 
+    /*
+    ============================DROP TABLE METHODS============================
+     */
 
+    /**
+     * Drops the given table if it exists.
+     * @param tableName The name of the table
+     * @return 1 if successful, 0 if failure
+     */
+    public int dropTable(String tableName) {
+        String update = "DROP TABLE IF EXISTS " + tableName + ";";
+        try {
+            execute(update, UPDATE);
+            return 1;
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return 0;
+    }
 
+    /*
+    ============================CREATE TABLE METHODS============================
+     */
+
+    /**
+     * Creates the given table.
+     * @param query SQL Code of table definition
+     * @return 1 for success, 0 for failure
+     */
+    public int createTable(String query) {
+        try {
+            execute(query, UPDATE);
+            return 1;
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return 0;
+    }
+
+    /*
+    ============================TABLE DEFINITIONS============================
+     */
+    public static final String CREATE_USERS = "CREATE TABLE IF NOT EXISTS Users(\n" +
+            "                      id INT,\n" +
+            "                      firstname VARCHAR(50) NOT NULL,\n" +
+            "                      lastname VARCHAR(50) NOT NULL,\n" +
+            "                      email VARCHAR(75) NOT NULL UNIQUE,\n" +
+            "                      password VARCHAR(20) NOT NULL,\n" +
+            "                      phone VARCHAR(20),\n" +
+            "                      address VARCHAR(100),\n" +
+            "                      gender VARCHAR(20),\n" +
+            "                      date_of_birth DATE,\n" +
+            "                      PRIMARY KEY (id),\n" +
+            "                      CHECK (date_of_birth < CURDATE() AND id > 0 ) );";
+
+    public static final String CREATE_EMPLOYEE = "CREATE TABLE IF NOT EXISTS Employee(\n" +
+            "    id INT,\n" +
+            "    salary NUMERIC(18, 2) NOT NULL,\n" +
+            "    employment_date DATE NOT NULL,\n" +
+            "    annual_leave INT NOT NULL,\n" +
+            "    PRIMARY KEY (id),\n" +
+            "    CHECK (salary >= 0 AND id > 0 AND annual_leave > 0));";
+
+    public static final String CREATE_RECEPTIONIST = "CREATE TABLE IF NOT EXISTS Receptionist(\n" +
+            "    id INT,\n" +
+            "    PRIMARY KEY (id),\n" +
+            "    FOREIGN KEY (id) REFERENCES Employee(id)\n" +
+            "        ON DELETE CASCADE\n" +
+            "        ON UPDATE CASCADE,\n" +
+            "    CHECK (id > 0));";
+
+    public static final String CREATE_CANDIDATE = "CREATE TABLE IF NOT EXISTS Candidate(\n" +
+            "    id INT,\n" +
+            "    cover_letter VARCHAR(5000) NOT NULL,\n" +
+            "    PRIMARY KEY (id),\n" +
+            "    FOREIGN KEY (id) REFERENCES Users(id)\n" +
+            "        ON DELETE CASCADE\n" +
+            "        ON UPDATE CASCADE,\n" +
+            "    CHECK (id > 0));";
+
+    public static final String CREATE_RECRUITER = "CREATE TABLE IF NOT EXISTS Recruiter(\n" +
+            "    id INT,\n" +
+            "    PRIMARY KEY (id),\n" +
+            "    FOREIGN KEY (id) REFERENCES Employee(id)\n" +
+            "        ON DELETE CASCADE\n" +
+            "        ON UPDATE CASCADE,\n" +
+            "    CHECK (id > 0));";
+
+    public static final String CREATE_SECURITY_STAFF = "CREATE TABLE IF NOT EXISTS Security_Staff(\n" +
+            "    id INT,\n" +
+            "    security_rank VARCHAR(20) NOT NULL,\n" +
+            "    weapon VARCHAR(20),\n" +
+            "    PRIMARY KEY (id),\n" +
+            "    FOREIGN KEY (id) REFERENCES Employee(id)\n" +
+            "        ON DELETE CASCADE\n" +
+            "        ON UPDATE CASCADE,\n" +
+            "    CHECK (id > 0));";
+
+    public static final String CREATE_MANAGER = "CREATE TABLE IF NOT EXISTS Manager(\n" +
+            "    id INT,\n" +
+            "    office_no VARCHAR(10) NOT NULL,\n" +
+            "    PRIMARY KEY (id),\n" +
+            "    FOREIGN KEY (id) REFERENCES Employee(id)\n" +
+            "        ON DELETE CASCADE\n" +
+            "        ON UPDATE CASCADE,\n" +
+            "    CHECK (id > 0) );";
+
+    public static final String CREATE_GUESTS = "CREATE TABLE IF NOT EXISTS Guests(\n" +
+            "    id INT,\n" +
+            "    money_spent NUMERIC(18, 2) NOT NULL,\n" +
+            "    PRIMARY KEY (id),\n" +
+            "    FOREIGN KEY (id) REFERENCES Users(id)\n" +
+            "        ON DELETE CASCADE\n" +
+            "        ON UPDATE CASCADE,\n" +
+            "    CHECK (id > 0 AND money_spent > 0));";
+
+    public static final String CREATE_HOUSEKEEPER = "CREATE TABLE IF NOT EXISTS Housekeeper(\n" +
+            "    id INT,\n" +
+            "    PRIMARY KEY (id),\n" +
+            "    FOREIGN KEY (id) REFERENCES Employee(id)\n" +
+            "        ON DELETE CASCADE\n" +
+            "        ON UPDATE CASCADE,\n" +
+            "    CHECK (id > 0));";
 }
--- HotelManagementBackend/src/main/java/com/example/HotelManagement/HotelManagementApplication.java
+++ HotelManagementBackend/src/main/java/com/example/HotelManagement/HotelManagementApplication.java
@@ -1,5 +1,6 @@
 package com.example.HotelManagement;
 
+import com.example.HotelManagement.Database.DatabaseConnection;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HotelManagementApplication {
 
 	public static void main(String[] args) {
+		DatabaseConnection databaseConnection = new DatabaseConnection();
+		/*
+		databaseConnection.createTable(DatabaseConnection.CREATE_EMPLOYEE);
+		databaseConnection.createTable(DatabaseConnection.CREATE_RECEPTIONIST);
+		databaseConnection.createTable(DatabaseConnection.CREATE_CANDIDATE);
+		databaseConnection.createTable(DatabaseConnection.CREATE_RECRUITER);
+		databaseConnection.createTable(DatabaseConnection.CREATE_SECURITY_STAFF);
+		databaseConnection.createTable(DatabaseConnection.CREATE_MANAGER);
+		databaseConnection.createTable(DatabaseConnection.CREATE_GUESTS);
+		databaseConnection.createTable(DatabaseConnection.CREATE_HOUSEKEEPER);
+		*/
 		SpringApplication.run(HotelManagementApplication.class, args);
 	}
 
