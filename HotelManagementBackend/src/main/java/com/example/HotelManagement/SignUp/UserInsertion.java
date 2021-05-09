99f912abde06f89f2fb14b90ae4dd2eac287f4ca
Bora Çün <cun2424@gmail.com>
2021-05-09 15:21:44 +0300

    Insertion ok

--- HotelManagementBackend/src/main/java/com/example/HotelManagement/SignUp/UserInsertion.java
+++ HotelManagementBackend/src/main/java/com/example/HotelManagement/SignUp/UserInsertion.java
@@ -1,6 +1,9 @@
 package com.example.HotelManagement.SignUp;
 
+import com.example.HotelManagement.DTO.MessageResponse;
+import com.example.HotelManagement.DTO.MessageType;
 import com.example.HotelManagement.Database.DatabaseConnection;
+import org.apache.logging.log4j.message.Message;
 
 import java.sql.ResultSet;
 
@@ -26,44 +29,50 @@ public class UserInsertion {
      * @param dateOfBirth date of birth as Long value
      * @return 1 if success, 0 if failure
      */
-    public int insertUser(String firstName, String lastName, String email, String password, String phone,
-                          String address, String gender, Long dateOfBirth) {
+    public MessageResponse insertUser(String firstName, String lastName, String email, String password, String phone,
+                                      String address, String gender, Long dateOfBirth) {
         String query;
         ResultSet rs;
         int id;
 
         //Preconditions
         if(firstName.length() > 50) {
-            throw new Exception("First name length cannot be more than 50 characters.");
+            return new MessageResponse("First name length cannot be more than 50 characters.", MessageType.ERROR);
         }
         if (lastName.length() > 50) {
-            throw new Exception("Last name length cannot be more than 50 characters.");
+            return new MessageResponse("Last name length cannot be more than 50 characters.", MessageType.ERROR);
         }
         if (email.length() > 75) {
-            throw new Exception("Email length cannot be more than 75 characters.");
+            return new MessageResponse("Email length cannot be more than 75 characters.", MessageType.ERROR);
         }
         if (password.length() > 20) {
-            throw new Exception("Password length cannot be more than 20 characters.");
+            return new MessageResponse("Password length cannot be more than 20 characters.", MessageType.ERROR);
         }
         if (phone.length() > 20) {
-            throw new Exception("Phone length cannot be more than 20 characters.");
+            return new MessageResponse("Phone length cannot be more than 20 characters.", MessageType.ERROR);
         }
         if (address.length() > 100) {
-            throw new Exception("Address length cannot be more than 100 characters.");
+            return new MessageResponse("Address length cannot be more than 100 characters.", MessageType.ERROR);
         }
         if (gender.length() > 20) {
-            throw new Exception("Gender length cannot be more than 20 characters.");
+            return new MessageResponse("Gender length cannot be more than 20 characters.", MessageType.ERROR);
         }
 
         //find an unused id
         do {
             id = (int) (Math.random() * 100000);
-            rs = userFetch.selectUserById(id);
-        } while (rs != null);
+            rs = userFetch.selectUserById(1);
+        } while (rs == null);
 
-        //method
-        query = "INSERT INTO Users VALUES (" + id + ", " + firstName + ", " + lastName + ", " + email + ", " +
-                password + ", " + phone + ", " + address + ", " + gender + ", " + dateOfBirth + ");";
-        return 1;
+        //insertion
+        query = "INSERT INTO Users VALUES (" + id + ", '" + firstName + "', '" + lastName + "', '" + email + "', '" +
+                password + "', '" + phone + "', '" + address + "', '" + gender + "', " + dateOfBirth + ");";
+        try {
+            databaseConnection.execute(query, DatabaseConnection.UPDATE);
+        }
+        catch (Exception e) {
+            return new MessageResponse("User insertion failed.", MessageType.ERROR);
+        }
+        return new MessageResponse("User insertion successful.", MessageType.SUCCESS);
     }
 }
